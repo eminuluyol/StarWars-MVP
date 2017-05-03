c975adc41937b5b6e60641e595fe7b29f3b9f5cc
eminuluyol <eminuluyol@gmail.com>
2017-05-03 14:33:01 +0200

    Add fragment args dependency

---
+++ app/src/main/java/com/taurus/androidtest/core/dialog/BaseDialogFragment.java
@@ -0,0 +1,29 @@
+package com.taurus.androidtest.core.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseDialogFragment extends DialogFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayoutResId(), container);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    protected abstract int getLayoutResId();
+}
